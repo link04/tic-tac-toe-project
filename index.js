b84e4b44e69befb9a6deaa4aae57ae75739ddc8c
Maximo Bautista <max.bautista97@gmail.com>
2019-03-15 19:58:53 -0400

    first commit basic structure

---
+++ index.js
@@ -0,0 +1,18 @@
+// const winner =  false;
+//
+//
+// const images = document.querySelectorAll('img');
+// while(!winner){
+//   images.forEach(img => {
+//
+// 	if (img.className == "top"  ) {
+// 		img.className = "top"
+//      } else if (img.className == "bottom"  ) {
+// 		img.className = "transparent"
+// 	 } else if (img.className == "transparent"  ) {
+// 		img.className = "transparent"
+// 	}
+//      }
+//
+// )
+// }
