b91772070b2e8b93b7b70c312125b83fd583b619
Maximo Bautista <max.bautista97@gmail.com>
2019-03-17 17:39:30 -0400

    completed basic game and winner announce logic, added table logic and fixed some css responsive styles

--- index.js
+++ index.js
@@ -4,7 +4,30 @@ const gameDiv = document.querySelector('div#gameDiv');
 const formTag = document.querySelector('form');
 
 let winner = false;
+let draw = false;
 let actualUser = null;
+
+const defaultGameObject = {
+  x:{
+    name:null,
+    victories:0
+  },
+  o:{
+    name:null,
+    victories:0
+  },
+  gameArrays:{
+    a:["", "", ""],
+    b:["", "", ""],
+    c:["", "", ""]
+  },
+  draws:0,
+  reset: () => {
+       this.prop1 = false;
+       this.prop2 = true;
+       this.prop3 = null;
+   }
+};
 
 let gameObject = {
   x:{
@@ -20,12 +43,18 @@ let gameObject = {
     b:["", "", ""],
     c:["", "", ""]
   },
-  draws:0
+  draws:0,
+  reset: () => {
+       this.prop1 = false;
+       this.prop2 = true;
+       this.prop3 = null;
+   }
 };
 
-
-const checkForWinnerArray = (array, currentValue) => {
-  return array.every(element => element == currentValue);
+const checkForWinnerArray = (array) => {
+  if(array.length == 3 && winner === false){
+    winner = array.split('').every(element => element == actualUser);
+  }
 };
 
 const displaySideDivs = () => {
@@ -34,6 +63,7 @@ const displaySideDivs = () => {
 };
 
 const displayActualPlayer = (userOnTurn) => {
+  document.getElementById('actualPlayerHeader').innerText = `It is ${gameObject[actualUser].name}'s Turn`
   document.querySelectorAll('div#actualPlayer img').forEach((img) => {
     if (img.id != userOnTurn) {
       img.hidden = true;
@@ -52,33 +82,63 @@ const displayGameBoard = () => {
     displaySideDivs();
 }
 
-const savePlayerInput = (userInput,divIdArray) => {
+// Need to remane this functuons to correct way
+const findXarrayValues = (i) => {
+  let valuesString;
 
-  gameObject.gameArrays[divIdArray[0]][divIdArray[1]] = userInput;
+  if(i == 0){
+    valuesString = Array.from("abc", x => {
+         let value = gameObject.gameArrays[x][i];
+         i++;
+    return value;
+    }).join('');
+  } else {
+    valuesString = Array.from("abc", x => {
+         let value = gameObject.gameArrays[x][i];
+         i--;
+    return value;
+    }).join('');
+  }
+
+  return valuesString;
+}
+
+const findAmountOfMoves = () => {
+  let moves = 0;
+
+  ["a","b","c"].forEach(letter => moves += gameObject.gameArrays[letter].join('').length);
+
+  return moves;
+}
+
+const savePlayerInput = (divIdArray) => {
+
+  gameObject.gameArrays[divIdArray[0]][divIdArray[1]] = actualUser;
 
   const lastMoveArrayHor = gameObject.gameArrays[divIdArray[0]].join('');
-  if(lastMoveArrayHor.length == 3){
-    winner = checkForWinnerArray(lastMoveArrayHor.split(''),userInput);
-  };
+  const lastMoveArrayVert = Array.from("abc", key => gameObject.gameArrays[key][divIdArray[1]]).join('');
+  const arrayAdecending = findXarrayValues(0);
+  const arrayAacending = findXarrayValues(2);
 
-  const lastMoveArrayVert = Array.from("abc", x => gameObject.gameArrays[x][divIdArray[1]]);
-  debugger
-  // "abc".forEach(letter => {
-  //
-  // });
+  if(lastMoveArrayHor.length == 3 || lastMoveArrayVert.length == 3 || arrayAdecending.length == 3 ||arrayAacending.length == 3){
+    checkForWinnerArray(lastMoveArrayHor);
+    checkForWinnerArray(lastMoveArrayVert);
+    checkForWinnerArray(arrayAdecending);
+    checkForWinnerArray(arrayAacending);
 
-  // 
-  //     gameObject.gameArrays[divIdArray[0]];
-  //
-  // if(lastMoveArrayVert.length == 3){
-  //   winner = checkForWinnerArray(lastMoveArrayVert.split(''),userInput);
-  // };
+    const amountOfMoves = findAmountOfMoves();
+    console.log(amountOfMoves);
 
 
+    if (amountOfMoves >= 8 && winner === false){
+      draw = true;
+      swal(`Oh snap!` ,  `Is a draw :c`,  "warning" );
+    }
+  }
 
-  debugger
-
-
+    if(winner === false) {
+        actualUser == "x" ? actualUser = "o" : actualUser = "x";
+    }
 };
 
 const makeGameMove = (divId,moveUser) => {
@@ -89,19 +149,24 @@ const makeGameMove = (divId,moveUser) => {
       img.className = "choosen";
     }
   });
-
-  if(winner === false) {
-      moveUser == "x" ? actualUser = "o" : actualUser = "x";
-  }
-
 };
+
+// const makeGameMove = (divId,moveUser) => {
+//   document.querySelectorAll(`div#${divId} img`).forEach((img) => {
+//     if (img.className != moveUser) {
+//       img.remove();
+//     } else {
+//       img.className = "choosen";
+//     }
+//   });
+// };
 
 document.querySelector("#myForm").addEventListener("submit", function(e){
   e.preventDefault();
   gameObject.x.name = e.target.xName.value;
   gameObject.o.name = e.target.oName.value;
   e.target.parentElement.remove();
-  swal ( "Begin!" ,  `${gameObject.x.name} Vs ${gameObject.o.name}`,  "success" );
+  swal( "Begin!" ,  `${gameObject.x.name} Vs ${gameObject.o.name}`,  "success" );
   displayGameBoard();
 
 });
@@ -110,16 +175,22 @@ document.addEventListener('click',(event => {
 
       if(actualUser != null){
         let targetDiv;
-        if (event.target.parentElement.className == "col-sm" ){
+        if (event.target.parentElement.className == "col tic-tac-toe" ){
           targetDiv = event.target.parentElement;
-        } else if (event.target.className == "col-sm" ){
+        } else if (event.target.className == "col tic-tac-toe" ){
           targetDiv = event.target;
         }
         if (targetDiv != undefined) {
           targetDiv.style.pointerEvents = "none";
-          savePlayerInput(actualUser,targetDiv.id.split(''));
           makeGameMove(targetDiv.id, actualUser);
-          displayActualPlayer(actualUser);
+          savePlayerInput(targetDiv.id.split(''));
+          if(winner === false){
+            displayActualPlayer(actualUser);
+          } else {
+            gameObject[actualUser].victories += 1;
+            swal( `${gameObject[actualUser].name} Won!` ,  `Victory #${gameObject[actualUser].victories}`,  "success" );
+          }
+
         }
       }
     }));
