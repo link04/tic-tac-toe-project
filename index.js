c06cf98ee34e81cced7184bb1e467f0645391096
Maximo Bautista <max.bautista97@gmail.com>
2019-03-17 19:42:51 -0400

    added restart match and continue with same stats funcitons and options

--- index.js
+++ index.js
@@ -21,12 +21,7 @@ const defaultGameObject = {
     b:["", "", ""],
     c:["", "", ""]
   },
-  draws:0,
-  reset: () => {
-       this.prop1 = false;
-       this.prop2 = true;
-       this.prop3 = null;
-   }
+  draws:0
 };
 
 let gameObject = {
@@ -43,12 +38,7 @@ let gameObject = {
     b:["", "", ""],
     c:["", "", ""]
   },
-  draws:0,
-  reset: () => {
-       this.prop1 = false;
-       this.prop2 = true;
-       this.prop3 = null;
-   }
+  draws:0
 };
 
 const checkForWinnerArray = (array) => {
@@ -60,6 +50,11 @@ const checkForWinnerArray = (array) => {
 const displaySideDivs = () => {
   document.getElementById('actualPlayer').hidden = false;
   document.getElementById('playersScore').hidden = false;
+};
+
+const hideSideDivs = () => {
+  document.getElementById('actualPlayer').hidden = true;
+  document.getElementById('playersScore').hidden = true;
 };
 
 const displayActualPlayer = (userOnTurn) => {
@@ -75,8 +70,9 @@ const displayActualPlayer = (userOnTurn) => {
 
 const displayGameBoard = () => {
     gameDiv.querySelectorAll('.row').forEach(div => div.style.display = "-webkit-flex");
-    const firstPlayer = Math.floor(Math.random() * Math.floor(2));
-    firstPlayer == 0 ? actualUser = "x" : actualUser = "o";
+
+      const firstPlayer = Math.floor(Math.random() * Math.floor(2));
+      firstPlayer == 0 ? actualUser = "x" : actualUser = "o";
 
     displayActualPlayer(actualUser);
     displaySideDivs();
@@ -127,46 +123,137 @@ const savePlayerInput = (divIdArray) => {
     checkForWinnerArray(arrayAacending);
 
     const amountOfMoves = findAmountOfMoves();
-    console.log(amountOfMoves);
-
 
     if (amountOfMoves >= 8 && winner === false){
       draw = true;
-      swal(`Oh snap!` ,  `Is a draw :c`,  "warning" );
     }
   }
 
-    if(winner === false) {
-        actualUser == "x" ? actualUser = "o" : actualUser = "x";
-    }
+  if(winner === false && draw === false) {
+      actualUser == "x" ? actualUser = "o" : actualUser = "x";
+  }
 };
 
 const makeGameMove = (divId,moveUser) => {
   document.querySelectorAll(`div#${divId} img`).forEach((img) => {
     if (img.className != moveUser) {
-      img.remove();
+      img.hidden = true;
     } else {
       img.className = "choosen";
     }
   });
 };
 
-// const makeGameMove = (divId,moveUser) => {
-//   document.querySelectorAll(`div#${divId} img`).forEach((img) => {
-//     if (img.className != moveUser) {
-//       img.remove();
-//     } else {
-//       img.className = "choosen";
-//     }
-//   });
-// };
+const setTableData = () => {
+
+  const trTags = document.querySelectorAll('table tbody tr');
+
+  trTags[0].children[1].innerText = gameObject.x.name;
+  trTags[0].children[2].innerText = gameObject.x.victories
+
+  trTags[1].children[1].innerText = gameObject.o.name;
+  trTags[1].children[2].innerText = gameObject.o.victories
+
+  trTags[2].children[2].innerText = gameObject.draws;
+
+    if( gameObject.x.victories > gameObject.o.victories){
+
+      trTags[0].children[0].innerHTML = `<i class="golden-star fas fa-star"></i>`;
+    } else if (  gameObject.x.victories < gameObject.o.victories) {
+      trTags[1].children[0].innerHTML = `<i class="golden-star fas fa-star"></i>`;
+    } else {
+      trTags[1].children[0].innerHTML = "";
+    }
+
+}
+
+const restartBoardState = () => {
+  delete gameObject.gameArrays;
+  gameObject.gameArrays = {
+    a:["", "", ""],
+    b:["", "", ""],
+    c:["", "", ""]
+  }
+  gameDiv.querySelectorAll('.row img').forEach(img => {
+    img.hidden = false;
+    img.className = img.name;
+    img.parentElement.style.pointerEvents = "";
+  });
+  setTableData();
+}
+
+const restartMatchData = () => {
+
+  gameObject = defaultGameObject;
+  restartBoardState();
+  gameDiv.querySelectorAll('.row').forEach(div => div.style.display = "none");
+  hideSideDivs();
+  document.querySelector("#myForm").parentElement.hidden = false;
+}
+
+const gameEndBehavior = () => {
+
+  if(winner === true ){
+    swal(`${gameObject[actualUser].name} Won!` ,  `Victory #${gameObject[actualUser].victories}`,  "success", {
+      buttons: {
+        cancel: "Restart Match Data",
+        continue: true,
+      }
+    }).then((value) => {
+      switch (value) {
+        case "continue":
+          swal("Great", "Will set up new match!", "success");
+          restartBoardState();
+          break;
+
+        default:
+        swal("Please play again! Bye!");
+        restartMatchData();
+      }
+    });
+  } else if(draw === true){
+    swal(`Oh snap!` ,  `Is a draw :(`,  "warning" , {
+      buttons: {
+        cancel: "Restart Match Data",
+        continue: true,
+      }
+    }).then((value) => {
+      switch (value) {
+        case "continue":
+          swal("Great", "Will set up new match!", "success");
+          restartBoardState();
+          break;
+
+        default:
+        swal("Please play again! Bye!");
+        restartMatchData();
+      }
+    });
+
+  }
+  winner = false;
+  draw = false;
+}
+
+const checkMatchStatus = () =>{
+    if(winner === false && draw === false){
+      displayActualPlayer(actualUser);
+    } else if(winner === true){
+      gameObject[actualUser].victories += 1;
+      gameEndBehavior();
+    } else if (draw === true){
+      gameObject.draws += 1;
+      gameEndBehavior();
+    }
+}
 
 document.querySelector("#myForm").addEventListener("submit", function(e){
   e.preventDefault();
   gameObject.x.name = e.target.xName.value;
   gameObject.o.name = e.target.oName.value;
-  e.target.parentElement.remove();
+  e.target.parentElement.hidden = true;
   swal( "Begin!" ,  `${gameObject.x.name} Vs ${gameObject.o.name}`,  "success" );
+  setTableData();
   displayGameBoard();
 
 });
@@ -184,13 +271,7 @@ document.addEventListener('click',(event => {
           targetDiv.style.pointerEvents = "none";
           makeGameMove(targetDiv.id, actualUser);
           savePlayerInput(targetDiv.id.split(''));
-          if(winner === false){
-            displayActualPlayer(actualUser);
-          } else {
-            gameObject[actualUser].victories += 1;
-            swal( `${gameObject[actualUser].name} Won!` ,  `Victory #${gameObject[actualUser].victories}`,  "success" );
-          }
-
+          checkMatchStatus();
         }
       }
     }));
